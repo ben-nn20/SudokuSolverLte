8ebbae464216bb33811db6bb67ae173e145fd779
Benjamin <Nakiwala>
2021-07-15 21:48:17 -0700

    Initial Commit

---
+++ SudokuSolverLte/SudokuSolverLteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuSolverLteApp.swift
+//  SudokuSolverLte
+//
+//  Created by Benjamin Nakiwala on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SudokuSolverLteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
